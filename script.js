7cd21979a6c4c150b145c9fe9b963f6bc17578d8
Giorgio Andrei <cipuandrei@libero.it>
2021-07-05 15:09:46 +0200

    numbers-generator-from-1-to-100-added

---
+++ script.js
@@ -0,0 +1,12 @@
+var x = '9';
+
+var numberList = [];
+
+var number = parseInt(0);
+
+for(i = 0; i < 100; i++){
+    number++;
+    console.log(number)
+    numberList = (number + 1).push;
+    console.log(numberList)
+}
