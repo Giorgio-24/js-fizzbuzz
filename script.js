2f36d802dac83082d0aa0ca3d8e83b1f4bad793a
Giorgio Andrei <cipuandrei@libero.it>
2021-07-05 16:07:37 +0200

    -

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for(i = 1; i < 101; i++){
     } else if(i % 3 == 0) {
         'Fizz'.push;
         document.write('  Fizz');
-        console.log('  Fizz')
+        console.log('Fizz')
     } else if( i % 5 == 0 ){
         'Buzz'.push;
         document.write('  Buzz');
