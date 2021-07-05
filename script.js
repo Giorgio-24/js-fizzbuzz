6ed113b631f4c633ead564c1cc3334d5ccf4ad84
Giorgio Andrei <cipuandrei@libero.it>
2021-07-05 17:07:34 +0200

    elements-displayed-vertically

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 for(i = 1; i < 101; i++){
     if(i % 3 == 0 && i % 5 == 0){
-        document.write('  FizzBuzz');
+        list.innerHTML += '<li>FizzBuzz</li>';
         console.log('FizzBuzz')
     } else if(i % 3 == 0) {
-        document.write('  Fizz');
+        list.innerHTML += '<li>Fizz</li>';
         console.log('Fizz')
     } else if( i % 5 == 0 ){
-        document.write('  Buzz');
+        list.innerHTML += '<li>Buzz</li>';
         console.log('Buzz')
     } else {
-        document.write('  ' + i);
+        list.innerHTML += `<li>${i}</li>`;
         console.log(i)
     }
 }
