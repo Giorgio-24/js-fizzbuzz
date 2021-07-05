ec3e307c782dd6597a058465805be51dae4bbbbe
Giorgio Andrei <cipuandrei@libero.it>
2021-07-05 15:40:35 +0200

    fizz-buzz-and-fizzbuzz-printed-in-console

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-var x = '9';
-
 var numberList = [];
 
-var number = parseInt(0);
-
-for(i = 0; i < 100; i++){
-    number++;
-    console.log(number)
-    numberList = (number + 1).push;
-    console.log(numberList)
+for(i = 1; i < 101; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        numberList = 'FizzBuzz'.push;
+        console.log('FizzBuzz')
+    } else if(i % 3 == 0) {
+    numberList = 'Fizz'.push;
+    console.log('Fizz')
+    } else if( i % 5 == 0 ){
+        numberList = 'Buzz'.push;
+        console.log('Buzz')
+    } else {
+        numberList = i.push;
+        console.log(i)
+    }
 }
