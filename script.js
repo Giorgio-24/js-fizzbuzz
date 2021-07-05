2a4a27c569e41e869a12082116e6c110c0e67432
Giorgio Andrei <cipuandrei@libero.it>
2021-07-05 16:09:05 +0200

    push-element-removed

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 for(i = 1; i < 101; i++){
     if(i % 3 == 0 && i % 5 == 0){
-        'FizzBuzz'.push;
         document.write('  FizzBuzz');
         console.log('FizzBuzz')
     } else if(i % 3 == 0) {
-        'Fizz'.push;
         document.write('  Fizz');
         console.log('Fizz')
     } else if( i % 5 == 0 ){
-        'Buzz'.push;
         document.write('  Buzz');
         console.log('Buzz')
     } else {
-        i.push;
         document.write('  ' + i);
         console.log(i)
     }
